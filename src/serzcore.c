18a1a8371ce97c55b550e180c1c2f2ac4f4d2f27
mightyrabbit99 <t.1999ken@gmail.com>
2026-01-23 22:16:38 +0800

    update license

--- src/serzcore.c
+++ src/serzcore.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 mightyrabbit99
+ * Copyright (c) 2025 Tan Wah Ken
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
--- src/serzcore.h
+++ src/serzcore.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 mightyrabbit99
+ * Copyright (c) 2025 Tan Wah Ken
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the MIT license. See `serzcore.c` for details.
